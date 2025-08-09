9ba1a1b161c05e01e5b89a28fb7b7d91970c0048
one-ccs <one-ccs@foxmail.com>
2025-08-09 19:28:48 +0800

    fix: 修复持久化值新旧不兼容导致页面白屏的问题

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,13 +7,14 @@ import router from './router';
 import piniaPersist from './plugins/piniaPersist';
 
 import 'vant/lib/index.css';
-
 import './assets/css/main.less';
-import './assets/css/light.less';
-import './assets/css/dark.less';
 
 const app = createApp(App);
 const pinia = createPinia();
+
+app.config.errorHandler = (err, instance, info) => {
+    console.error(err, instance, info);
+};
 
 pinia.use(piniaPersist);
 
